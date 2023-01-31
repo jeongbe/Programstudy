509298431b8bbba009e3a32ba12cfcb9eeb91c23
jeongbe <101166792+jeongbe@users.noreply.github.com>
2023-01-31 23:16:42 +0900

    [Bronze V] Title: ??!, Time: 216 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String arge[]){
+        Scanner scan = new Scanner(System.in);
+        
+        String id = scan.nextLine();
+        
+        System.out.println(id+"??!");
+        
+        scan.close();
+    }
+}

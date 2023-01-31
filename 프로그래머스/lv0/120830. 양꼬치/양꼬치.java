9e7964c498ad33c01ec5fa73828222d1fb18df9c
jeongbe <101166792+jeongbe@users.noreply.github.com>
2023-01-31 18:01:27 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        int juice = 0;
+        
+        
+        
+        
+        answer = (n*12000) + (k*2000) - (n/10 *2000);
+        
+        return answer;
+    }
+}

db005e5bb506441f4ad77672b56ca5db7027e43e
jeongbe <101166792+jeongbe@users.noreply.github.com>
2023-02-12 01:42:53 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 304 ms, Memory: 18044 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String arge[]){
+        Scanner scan = new Scanner(System.in);
+        
+        int n = scan.nextInt();
+
+        for (int i = 0; i < n; i++) {
+
+          for (int j = 0; j <= i; j++) {
+            System.out.print("*");
+            }
+          System.out.println();
+           }
+
+         
+    }
+}

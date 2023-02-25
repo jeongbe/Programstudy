aac6b92be31359713a51f9f3e47098c21c6f5506
jeongbe <101166792+jeongbe@users.noreply.github.com>
2023-02-26 00:44:43 +0900

    [Bronze II] Title: 공 바꾸기, Time: 244 ms, Memory: 18544 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main{
+
+
+public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    int n = scan.nextInt(); //바구니 개수
+    int m = scan.nextInt(); 
+    int[] arr = new int[n];
+    int k = 1;
+
+    for(int a = 0; a < n; a++){
+        arr[a] = k;
+        k++;
+    }
+
+    for(int b = 0; b < m; b++) {
+        int i = scan.nextInt();
+        int j = scan.nextInt();
+        
+        i = i-1;
+        j = j-1;
+        int s = 0; 
+        s = arr[i];
+        arr[i] = arr[j];
+        arr[j] = s;     
+    }
+    
+    for(int c = 0; c < n; c++) {
+        System.out.print(arr[c]+" ");
+    }
+     scan.close();
+}
+}

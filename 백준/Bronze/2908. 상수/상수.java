6c67dfa943af6ad476f86edcc3f02bb996c0df6d
jeongbe <101166792+jeongbe@users.noreply.github.com>
2023-03-30 01:32:32 +0900

    [Bronze II] Title: 상수, Time: 200 ms, Memory: 17532 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -2,19 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner scan = new Scanner(System.in);
 
-        int a = scan.nextInt();
-        int b = scan.nextInt();
+        String a = scan.next();
+        String b = scan.next();
         scan.close();
 
-        String aa = Integer.toString(a);
-        String t1 = new StringBuffer(aa).reverse().toString();
+        String t1 = new StringBuffer(a).reverse().toString();
         int a1 = Integer.parseInt(t1);
 
-        String bb = Integer.toString(b);
-        String t2 = new StringBuffer(bb).reverse().toString();
+        String t2 = new StringBuffer(b).reverse().toString();
         int b1 = Integer.parseInt(t2);
 
         if (a1 > b1) {

b9cab9f6177e5ce02718f031686c60fd93f00493
jeongbe <101166792+jeongbe@users.noreply.github.com>
2023-09-12 17:10:48 +0900

    [level 2] Title: 올바른 괄호, Time: 7.40 ms, Memory: 52.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,37 @@
+
+import java.util.Stack;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = false;
+        
+        int count = 0;
+        
+        for(int i = 0; i < s.length(); i++) {
+        	if(s.charAt(i) == '(') {
+        		count++;
+        	}
+        	
+        	if(s.charAt(i) == ')') {
+        		count--;
+        	}
+        	
+        	if(count < 0 ) {
+        		answer = false;
+        		break;
+        	}
+        }
+        
+        if(count == 0) {
+        	answer = true;
+        }
+
+        
+       
+        System.out.println(answer);
+        
+        return answer;
+    
+    }
+        
+}
